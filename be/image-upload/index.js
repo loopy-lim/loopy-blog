c7b5119efcba384fc1c329f37caed8c41cfba287
loopy-lim <loopy@jnu.ac.kr>
2022-12-24 23:40:10 +0900

    feat(dev): add develop watch develop script

--- be/image-upload/index.js
+++ be/image-upload/index.js
@@ -1,24 +1,41 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 const multer = require('multer');
 
 const upload = multer({
-  dest: __dirname + '/../nginx/images',
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, __dirname + '/../nginx/images');
+    },
+    filename: (req, file, done) => {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + ext);
+    },
+    fileFilter: (req, file, done) => {
+      const ext = path.extname(file.originalname);
+      if (
+        ext !== '.png' &&
+        ext !== '.png' &&
+        ext !== '.gif' &&
+        ext !== '.jpeg'
+      ) {
+        return done(res.end('Only images are allwed'), null);
+      }
+      done(null, true);
+    },
+  }),
 });
 
 app.use(cors());
 
-app.post('/:filename', async (req, res) => {
-  upload(req, res).then(
-    file => {
-      res.json(file);
-    },
-    err => {
-      res.send(500, err);
-    },
-  );
+app.post('/upload', upload.single('image'), (req, res) => {
+  res.status(200).send({
+    message: 'Ok',
+    filename: req.file.originalname,
+  });
 });
 
 app.listen(3003, () => console.log('Multer server start on 3003'));
